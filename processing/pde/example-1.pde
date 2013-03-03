80e556beaf8b6f9f9808859f0b5b77159429dda1
Matt Rudder <matt@mattrudder.com>
2013-03-02 20:47:33 -0800

    Framework for Processing experiments.

---
+++ processing/pde/example-1.pde
@@ -0,0 +1,46 @@
+/// Global variables
+float radius = 50.0;
+int X, Y;
+int nX, nY;
+int delay = 16;
+
+void clear() {
+  loadPixels();
+  for (int i = 0; i < width * height; i++) pixels[i] = 0;
+  updatePixels(); 
+}
+
+
+// Setup the Processing Canvas
+void setup(){
+  size( 400, 400 );
+  frameRate( 24 );
+  X = width / 2;
+  Y = height / 2;
+  nX = X;
+  nY = Y;  
+  
+  clear();
+}
+
+// Main draw loop
+void draw() {
+  
+  radius = radius + sin( frameCount / 14 ) * 8;
+  strokeWeight( radius / 10 );
+  
+  // Track circle to new destination
+  X+=(nX-X)/delay;
+  Y+=(nY-Y)/delay;
+  
+  // Fill canvas grey
+  clear();
+  
+  // Set fill-color to blue
+  fill( cos(frameCount / 16) * 128, sin(frameCount / 8) * 128, 255);
+  
+  // Set stroke-color white
+  stroke(255); 
+
+  ellipse( X, Y, radius, radius );                  
+}
--- processing/plugin/codemirror/codemirror-plugin.js
+++ processing/plugin/codemirror/codemirror-plugin.js
@@ -3,17 +3,81 @@
         throw 'The reveal.js CodeMirror plugin requires CodeMirror to be loaded';
     }
 
+    var editors = [];
+
+    var findSection = function(el) {
+        var parent = el.parentNode;
+
+        while (parent) {
+            if (parent.tagName == "SECTION") {
+                return parent;
+            }
+
+            parent = parent.parentNode;
+        }
+
+        return null;
+    }
+
+    var createEditor = function (textarea, index) {
+        var editor,
+            section,
+            options = {
+                theme: 'solarized dark',
+                lineNumbers: true,
+                lineWrapping: true
+            };
+
+        editor = CodeMirror.fromTextArea(textarea, options);
+        section = findSection(textarea);
+        if (section) {
+            section.setAttribute("data-codemirror-editor", index);
+            editors.push(editor);
+        }
+    }
+
     var initEditors = function () {
     	var textareas = document.querySelectorAll('textarea[data-codemirror]'),
-    		editor;
+            textarea,
+            url,
+            section;
 
     	for (var i = 0, iLen = textareas.length; i < iLen; ++i) {
-    		editor = CodeMirror.fromTextArea(textareas[i], {
-    			theme: 'solarized dark',
-    			lineNumbers: true,
-    			lineWrapping: true
-    		});
+            textarea = textareas[i];
+            url = textarea.getAttribute('data-codemirror');
+
+            if (url) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", url, false);
+                xhr.send(null);
+                textarea.value = xhr.responseText;
+                
+                createEditor(textarea, i);
+            }
+            else {
+                createEditor(textarea, i);
+            }
     	}
+
+        Reveal.getCodeMirrorOnSlide = function (slide) {
+            if (!!slide && slide.getAttribute) {
+                var editorIndex = parseInt(slide.getAttribute("data-codemirror-editor"));
+                if (!isNaN(editorIndex)) {
+                    return editors[editorIndex];
+                }
+            }
+
+            return null;
+        };
+
+        Reveal.addEventListener('slidechanged', function(event) {
+            var editor = Reveal.getCodeMirrorOnSlide(event.currentSlide);
+            if (editor) {
+                setTimeout(function () {
+                    editor.refresh();
+                }, 750);
+            }
+        });
     };
 
     initEditors();
